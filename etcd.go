627813f288b05c5ea6c47d218d2c71d546df2765
Scott Barr <scottjbarr@gmail.com>
2015-08-06 01:04:53 +1000

    gofmt

--- etcd.go
+++ etcd.go
@@ -1,36 +1,34 @@
 package main
 
-import(
-    "github.com/coreos/go-etcd/etcd"
-    "strings"
-    "fmt"
-    "log"
+import (
+	"fmt"
+	"github.com/coreos/go-etcd/etcd"
+	"log"
+	"strings"
 )
 
-type Backend struct{
-    Name string
-    Ip string
-    Port string
+type Backend struct {
+	Name string
+	Ip   string
+	Port string
 }
 
+func GetBackends(client *etcd.Client, service, backendName string) ([]Backend, error) {
 
-func GetBackends(client *etcd.Client, service, backendName string)([]Backend,error){
+	resp, err := client.Get(service, false, true)
+	if err != nil {
+		log.Println("Error when reading etcd: ", err)
+		return nil, err
+	} else {
+		backends := make([]Backend, len(resp.Node.Nodes))
+		for index, element := range resp.Node.Nodes {
 
-    resp,err := client.Get(service,false,true)
-    if (err != nil){
-        log.Println("Error when reading etcd: ",err)
-        return nil,err
-    } else{
-        backends := make([]Backend,len(resp.Node.Nodes))
-        for index,element := range resp.Node.Nodes {
+			key := (*element).Key // key format is: /service/IP:PORT
+			service := strings.Split(key[strings.LastIndex(key, "/")+1:], ":")
 
-            key := (*element).Key // key format is: /service/IP:PORT
-            service := strings.Split(key[strings.LastIndex(key,"/")+1:],":")
-
-            backends[index] = Backend{Name: fmt.Sprintf("back-%v",index), Ip: service[0], Port: service[1]}
-        }
-        return backends,nil
-    }
+			backends[index] = Backend{Name: fmt.Sprintf("back-%v", index), Ip: service[0], Port: service[1]}
+		}
+		return backends, nil
+	}
 
 }
-
--- haproxy.go
+++ haproxy.go
@@ -1,46 +1,46 @@
 package main
 
-import(
-    "text/template"
-    "os"
-    "os/exec"
-    "log"
-    "strconv"
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"text/template"
 )
 
 var tpl *template.Template = nil
 var pid int = -1
 
-func createConfigFile(backends []Backend, templateFile, outputFile string)(error){
-    cfgFile,_ := os.Create(outputFile)
-    defer cfgFile.Close()
+func createConfigFile(backends []Backend, templateFile, outputFile string) error {
+	cfgFile, _ := os.Create(outputFile)
+	defer cfgFile.Close()
 
-    if(tpl == nil){
-        var err error = nil
-        tpl, err = template.ParseFiles(templateFile)
-        if (err != nil){
-            return err
-        }
-    }
-    
-    return tpl.Execute(cfgFile, backends)
+	if tpl == nil {
+		var err error = nil
+		tpl, err = template.ParseFiles(templateFile)
+		if err != nil {
+			return err
+		}
+	}
+
+	return tpl.Execute(cfgFile, backends)
 }
 
-func reloadHAproxy(command, configFile string)(error){
-    var cmd *exec.Cmd = nil
-    if pid == -1{
-        log.Println("Start HAproxy")
-        cmd = exec.Command(command,"-f",configFile)
-        go cmd.Wait()
-    } else{
-        log.Println("Restart HAproxy")
-        cmd = exec.Command(command,"-f",configFile,"-sf",strconv.Itoa(pid))
-    }
-    
-    err := cmd.Start()
-    if (err == nil){
-        pid = cmd.Process.Pid
-        log.Println("New pid: ",pid)
-    }
-    return err
+func reloadHAproxy(command, configFile string) error {
+	var cmd *exec.Cmd = nil
+	if pid == -1 {
+		log.Println("Start HAproxy")
+		cmd = exec.Command(command, "-f", configFile)
+		go cmd.Wait()
+	} else {
+		log.Println("Restart HAproxy")
+		cmd = exec.Command(command, "-f", configFile, "-sf", strconv.Itoa(pid))
+	}
+
+	err := cmd.Start()
+	if err == nil {
+		pid = cmd.Process.Pid
+		log.Println("New pid: ", pid)
+	}
+	return err
 }
